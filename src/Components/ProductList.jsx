7d53674122b30f811080a937784edcc395f46c3d
JolisNzamb21st <nzambimanafranck@gmail.com>
2025-10-31 10:50:37 -0400

    add store component for global state

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ProductList.css'; 
-
+import { useDispatch,useSelector } from 'react-redux';
+import { addItemToCart } from './CartSlice';
 const ProductList = () => {
 
   const products = [
@@ -8,7 +9,12 @@ const ProductList = () => {
     { id: 2, name: 'Product B', price: 75 },
     { id: 3, name: 'Product C', price: 30 },
   ];
-
+  const dispatch = useDispatch();
+  const cartItems = useSelector(state.cart.cartItems);
+  
+  const handleAddToCart = (product) => {
+    dispatch(addItemToCart(product));
+  }
   return (
     <div className="product-list">
       <h2 className="product-list-title">Products</h2>
